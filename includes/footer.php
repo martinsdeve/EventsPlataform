6f64050e3b92be2b479831b123f50d5e4a89c7aa
matheus gomes <matheusgomesdeveloper38@gmail.com>
2024-11-02 11:51:57 -0500

    feat -criação do header e estilização do header main e footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,3 @@
-<p>&copy; EventsPlataform. Todos os direitos reservados. Nenhuma parte deste material pode ser reproduzida ou distribuída sem a permissão expressa do autor.</p>
+<div id="footer">
+<p>&copy; <abbr class="tooltip" title="A EventsPlatform é uma plataforma completa para gerenciar, promover e organizar eventos, oferecendo soluções integradas para inscrições, pagamentos e comunicação com participantes.">EventsPlataform</abbr>. Todos os direitos reservados. Nenhuma parte deste material pode ser reproduzida ou distribuída sem a permissão expressa do autor.</p>
+</div>
